883b9d048cf3fdb700f67b9d66f34906216ee9a7
cmccreadie1 <50778220+cmccreadie1@users.noreply.github.com>
2025-03-16 19:01:03 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,19 @@
-const CACHE_NAME = 'simple-pwa-cache-v1';
+const CACHE_NAME = 'sea-catch-scorer-cache-v1';
 const urlsToCache = [
   '.',
   'index.html',
   'manifest.json',
-  'icon.png'
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js',
+  'https://i.postimg.cc/pLFtxQWs/seacatchscorecard.jpg'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
+        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
