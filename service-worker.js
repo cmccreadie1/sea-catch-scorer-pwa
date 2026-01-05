0c340c10381c9a1617cff3ebd975fcfd97b8833a
Sea Catch Scorer <50778220+cmccreadie1@users.noreply.github.com>
2026-01-05 13:32:55 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,6 +3,7 @@ const FILES_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
+  './icon.png',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
   'https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css',
   'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Poppins:wght@500;600;700;800&display=swap',
